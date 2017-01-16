9d3659b7d2f42a53bea9af1168bda46c578dfd58
Artem <Artem@Danils-MacBook-Pro.local>
2017-01-16 16:27:43 +0200

    iTimofeev. Added loading status, ability to post something

--- MonsterFeed/AppDelegate.swift
+++ MonsterFeed/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
+    var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+    var container: UIView!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
@@ -23,6 +25,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    
+    class func instance() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    func showActivityIndicator() {
+        if let window = window {
+             self.container = UIView()
+            self.container.frame = window.frame
+            self.container.center = window.center
+            self.container.backgroundColor = UIColor(white: 0, alpha: 0.8)
+            
+            self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            self.activityIndicator.hidesWhenStopped = true
+            self.activityIndicator.center = CGPoint(x: self.container.frame.size.width/2, y: self.container.frame.size.height/2)
+            
+            self.container.addSubview(self.activityIndicator)
+            self.window?.addSubview(self.container)
+            
+            self.activityIndicator.startAnimating()
+        }
+    }
+    
+    func dismissActivityIndicator() {
+        if let _ = self.window {
+            self.container.removeFromSuperview()
+        }
+    }
+    
+    
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ MonsterFeed/UploadViewController.swift
@@ -0,0 +1,91 @@
+//
+//  UploadViewController.swift
+//  MonsterFeed
+//
+//  Created by Artem on 1/15/17.
+//  Copyright © 2017 ApiqA. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var selectImageButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    var picker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        picker.delegate = self
+
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.imageView.image = image
+            self.selectImageButton.isHidden = true
+            self.postButton.isHidden = false
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func postButtonHandler(_ sender: UIButton) {
+        AppDelegate.instance().showActivityIndicator()
+        
+        let uid = FIRAuth.auth()!.currentUser!.uid
+        let ref = FIRDatabase.database().reference()
+        let storage = FIRStorage.storage().reference(forURL: "gs://test-13497.appspot.com")
+        
+        let key = ref.child("posts").childByAutoId().key
+        let imageRef = storage.child("posts").child(uid).child("\(key).jpg")
+        
+        let data = UIImageJPEGRepresentation(self.imageView.image!, 0.6)
+        
+        let uploadTask = imageRef.put(data!, metadata: nil, completion: { (metadata, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+                AppDelegate.instance().dismissActivityIndicator()
+                return
+            }
+            
+            imageRef.downloadURL(completion: { (url, error) in
+                if let url = url {
+                    
+                    let feed = ["userId" : uid,
+                                "pathToImage" : url.absoluteString,
+                                "likes" : 0,
+                                "author" : FIRAuth.auth()?.currentUser!.displayName!,
+                                "postID" : key] as [String : Any]
+                    
+                    let postFeed = ["\(key)" : feed]
+                    
+                    ref.child("posts").updateChildValues(postFeed)
+                    
+                    AppDelegate.instance().dismissActivityIndicator()
+                    
+                    self.dismiss(animated: true, completion: nil)
+                }
+            })
+        })
+        uploadTask.resume()
+    }
+    
+    @IBAction func selectImageButtonHandler(_ sender: UIButton) {
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        
+        self.present(picker, animated: true, completion:  nil)
+    }
+    
+
+}
